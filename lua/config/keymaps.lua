cd86dd470846afbb2c103e51e2b4763c22bc1156
Zhuanz <Zhuanz@MacBook-Pro.local>
2025-11-18 10:04:34 +0800

    update: add terminal keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -42,3 +42,14 @@ map("v", ">", ">gv", opt)
 -- lspsage
 map("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opt) -- rename
 map("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opt) -- code action
+
+-- terminal
+map("n", "<leader>th", ":split | terminal<CR>", { noremap = true, silent = true })
+map("n", "<leader>tv", ":vsplit | terminal<CR>", { noremap = true, silent = true })
+map("n", "<leader>tf", ":tabnew | terminal<CR>", { noremap = true, silent = true })
+
+map("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
+map("t", "<C-h>", "<C-\\><C-n><C-w>h", { noremap = true })
+map("t", "<C-j>", "<C-\\><C-n><C-w>j", { noremap = true })
+map("t", "<C-k>", "<C-\\><C-n><C-w>k", { noremap = true })
+map("t", "<C-l>", "<C-\\><C-n><C-w>l", { noremap = true })
